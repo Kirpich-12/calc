2b9776a037c4acb1051f9075b86f2db36572dbad
Kirpich-12 <ponchik34@tuta.io>
2024-11-29 15:53:44 +0300

    blt

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 #ввод данных
 #4 функции(+-/*)
 vvod = input()
-
+operants = []
 
 def addition(a:int, b:int) -> int:
     return a + b
@@ -10,37 +10,65 @@ def subtraction(a:int, b:int) -> int:
     return a - b
 
 def multiplication(a:int, b:int) -> int:
+    if a == 0:
+        a = 1
     return a * b
 
 def division(a:int, b:int) -> int:
+    if a == 0:
+        a = 1
     return a / b
 
 
 def sepor(string:str, seporator:str) -> list:
+    ans_s =[]
     ans = string.split(seporator)
     for i in range(len(ans)):
-        ans[i] = float(ans[i])
-    return ans
+        if ans[i] != '':
+            ans_s.append(ans[i])            
+    for i in range(len(ans_s)):
+        ans_s[i] = float(ans_s[i])
+    return ans_s
 
-#TODO отделить простые дейстивия от сложнных  
-if '+' in vvod:
-    numbers = sepor(vvod, '+')
-    if len(numbers) == 2:
-        print(addition(numbers[0], numbers[1]))
-elif '-' in vvod:
-    numbers = sepor(vvod, '-')
-    if len(numbers) == 2:
-        print(subtraction(numbers[0], numbers[1]))
-elif '*' in vvod:
-    numbers = sepor(vvod, '*')
-    if len(numbers) == 2:
-        print(multiplication(numbers[0], numbers[1]))
-elif '/' in vvod:
-    numbers = sepor(vvod, '/')
-    if len(numbers) == 2:
-        print(division(numbers[0], numbers[1]))
-else:
-    print('Неверный ввод, попробуйте ещё раз')
 
+
+
+while ('-' in vvod) or ('+' in vvod) or ('*'in vvod)  or ('/' in vvod):
+    print('True')
+    for i in vvod :                     #Разделение строки на числа
+        if i =='+' or i=='-' or i=='*' or i=='/':
+            operants.append(i)
+            vvod = vvod.replace(i, '')
+    numbers = vvod
+    print(vvod)
+    print(operants)
+
+list_num = sepor(numbers, ' ')
+
+amount = len(list_num)
+final_i = 0
+print(list_num)
+
+fn = 1
+n2 = numbers[0]
+
+for operant in operants:
+    i = 1
+    if operant == '+':
+        fn = addition(fn, list_num[i])
+        i += 1
+    elif operant == '-':
+        fn = subtraction(fn, list_num[i])
+        i += 1
+    elif operant == '*':
+        fn = subtraction(fn, list_num[i])
+        i += 1
+    elif operant == '/':
+        fn = division(fn, list_num[i])
+        i += 1
+    print(fn)
+print(fn)
+        
+        
 
 
---
+++ tt.py
@@ -0,0 +1,3 @@
+vvod = '5                   +                    5                        +                 5'
+a = ('-' in vvod) or ('+' in vvod) or ('*'in vvod)  or ('/' in vvod)
+print(a)
